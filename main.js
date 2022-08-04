bf67035eeba29b31e8af5c612aeb5ec69d5fcfc0
nirmalpaul383 <nirmalpaul383>
2022-08-05 02:34:00 +0530

    bug fixes

--- main.js
+++ main.js
@@ -6,25 +6,28 @@ This project is originally made by me(N Paul).My github profile https: //github.
 
 **/
 
+//Declaration of variables
+let table, rows, rowLenght, column, columnLenght, oldDB, newDB
 
-{ //This block of code cotaints all essentials variables
+
+{ //This block of code cotaints all essentials variables's value
 
   //Selecting table element by 'table' id
-  var table = document.getElementById('table');
+  table = document.getElementById('table');
 
   //Set all rows name (A,B,C ... Z)
-  var rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var rowLenght = rows.length;
+  rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  rowLenght = rows.length;
 
   //Set all column name (1,2,3 ... 20)
-  var column = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+  column = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
     26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
-  var columnLenght = column.length;
+  columnLenght = column.length;
 
   //Database(Object) Declearing for keeping old(unsolved) values
-  var oldDB = {};
+  oldDB = {};
   //Database(Object) Declearing for keeping new(solved) values
-  var newDB = {};
+  newDB = {};
 
 
 }
